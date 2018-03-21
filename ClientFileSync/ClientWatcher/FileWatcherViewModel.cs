40d6bdcbd59165de1aabc54f0bb136a01482c877
Adrián <determinista@gmail.com>
2018-03-21 13:48:54 -0400

    Refactorizacion a MVVM en cliente WPF. Arreglos para que el borrado funcione bien en el cliente Web

---
+++ ClientFileSync/ClientWatcher/FileWatcherViewModel.cs
@@ -0,0 +1,220 @@
+﻿using ClientWatcher.Models;
+using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace ClientWatcher
+{
+    public class FileWatcherViewModel:INotifyPropertyChanged
+    {
+        private string _syncFolder = ConfigurationManager.AppSettings["SyncFolder"].ToString();
+        private string _webApiURLtoLoad = ConfigurationManager.AppSettings["WepApiURLtoLoad"].ToString();
+        private string _wepApiURLtoDownload = ConfigurationManager.AppSettings["WepApiURLtoDownload"].ToString();
+        private string _wepApiURLExistsFile = ConfigurationManager.AppSettings["WepApiURLExistsFile"].ToString();
+        private string _webApiURLtoDelete = ConfigurationManager.AppSettings["WepApiURLtoDelete"].ToString();
+        private string _singalRHost = ConfigurationManager.AppSettings["SignalRHost"].ToString();
+        FileSystemWatcher _watcher;
+
+        public System.Threading.Thread Thread { get; set; }
+        public IHubProxy Proxy { get; set; }
+        public HubConnection Connection { get; set; }
+        public bool Active { get; set; }
+
+        UIGeneralModel model;
+        public UIGeneralModel Model
+        {
+            get => model;
+            set
+            {
+                model = value;
+                RaisePropertyChanged("Model");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public FileWatcherViewModel()
+        {
+            Model = new UIGeneralModel();
+
+            InitializeWatcher();
+
+            StartHub();
+        }
+
+        private void StartHub()
+        {
+            Active = true;
+            Thread = new System.Threading.Thread(() =>
+            {
+                Connection = new HubConnection(_singalRHost);
+                Connection.Error += Connection_Error;
+                Connection.StateChanged += Connection_StateChanged;
+                Proxy = Connection.CreateHubProxy("FileSyncHub");
+
+                Proxy.On<string, string>("NewFileNotification", (fileName, CRC) => OnNewFileNotified(fileName, CRC));
+
+                Connection.Start();
+
+                while (Active)
+                {
+                    System.Threading.Thread.Sleep(10);
+                }
+            })
+            { IsBackground = true };
+            Thread.Start();
+        }
+
+        private void InitializeWatcher()
+        {
+            _watcher = new FileSystemWatcher();
+            _watcher.Path = _syncFolder;
+            //_watcher.NotifyFilter = NotifyFilters.Created;
+            _watcher.Filter = "*.*";
+            //_watcher.Changed += new FileSystemEventHandler(OnWatcherChanged);
+            _watcher.Deleted += new FileSystemEventHandler(OnWatcherDeleted);
+            _watcher.Created += new FileSystemEventHandler(OnWatcherCreated);
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnWatcherCreated(object source, FileSystemEventArgs e)
+        {
+            //if (!fileExistsOnServer(e.Name, "").Result)
+            AddEventToCollection(e);
+            Task.WaitAll(sendFile(e.Name));
+        }
+
+        private void AddEventToCollection(FileSystemEventArgs e)
+        {
+            var events = Model.FileEvents;
+            events.Add(new FileEvent() { Date = DateTime.Now, EventDescription = e.ChangeType + ": " + e.FullPath, Filename = e.Name });
+            Model.FileEvents = events;
+        }
+
+        private async Task sendFile(string fileName)
+        {
+            byte[] byteFile = tryToReadFile(_syncFolder + "\\" + fileName);
+            HttpContent fileContent = new ByteArrayContent(byteFile);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.PostAsync(_webApiURLtoLoad, new MultipartFormDataContent()
+                {
+                    { new StringContent(fileName),"fileName"},
+                    { fileContent, "file", fileName }
+                });
+            }
+        }
+
+        private byte[] tryToReadFile(string file, int tries = 0)
+        {
+            if (tries == 100)
+                throw new Exception("Can't access file " + file);
+
+            byte[] b;
+            try
+            {
+                b = System.IO.File.ReadAllBytes(file);
+            }
+            catch (IOException)
+            {
+                Thread.Sleep(500);
+                return tryToReadFile(file, ++tries);
+            }
+
+            return b;
+        }
+
+        private void OnWatcherDeleted(object source, FileSystemEventArgs e)
+        {
+            AddEventToCollection(e);
+            Task.WaitAll(deleteFileOnServer(e.Name));
+        }
+
+        private async Task deleteFileOnServer(string fileName)
+        {
+            var lastDotIndex = fileName.LastIndexOf('.');
+            var nameLength = lastDotIndex >= 0 ? lastDotIndex : fileName.Length;
+            var extensionLength = fileName.Length - 1 - nameLength;
+            var name = fileName.Substring(0, nameLength);
+
+            string extension;
+            if (lastDotIndex < fileName.Length - 1)
+                extension = fileName.Substring(lastDotIndex + 1, extensionLength);
+            else
+                extension = "";
+            using (var client = new HttpClient())
+            {
+                var response = await client.DeleteAsync(_webApiURLtoDelete + "/" + name + "/" + extension);
+            }
+        }
+
+        private void OnNewFileNotified(string fileName, string CRC)
+        {
+            //Check if I have the File
+            if (!alreadyHaveFile(fileName, CRC))
+                //Download the New File
+                getFile(fileName);
+        }
+
+        private bool alreadyHaveFile(string fileName, string CRC)
+        {
+            //Include CRC check ************************************************************************** TO DO
+            return File.Exists(_syncFolder + "\\" + fileName);
+        }
+
+        private async void getFile(string fileName)
+        {
+            using (var client = new HttpClient())
+            {
+                var responseStream = await client.GetStreamAsync(_wepApiURLtoDownload + "?fileName=" + fileName);
+                using (var fileWritter = File.Create(_syncFolder + "\\" + fileName))
+                {
+                    //responseStream.Seek(0, SeekOrigin.Begin);
+                    responseStream.CopyTo(fileWritter);
+                }
+            }
+        }
+
+        private void Connection_StateChanged(StateChange obj)
+        {
+            model.SignalRStatus = "Status: " + obj.NewState;
+        }
+
+        private void Connection_Error(Exception obj)
+        {
+            model.SignalRStatus = "Connection error: " + obj.ToString();
+        }
+
+        private async Task<bool> fileExistsOnServer(string fileName, string CRC)
+        {
+            bool final;
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(_wepApiURLExistsFile + "?fileName=" + fileName);
+                var result = await response.Content.ReadAsStringAsync();
+                final = JsonConvert.DeserializeObject<bool>(result);
+            }
+
+            return final;
+        }
+    }
+}
--- ClientFileSync/ClientWatcher/MainWindow.xaml.cs
+++ ClientFileSync/ClientWatcher/MainWindow.xaml.cs
@@ -28,158 +28,158 @@ namespace ClientWatcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string _syncFolder = ConfigurationManager.AppSettings["SyncFolder"].ToString();
-        private string _webApiURLtoLoad = ConfigurationManager.AppSettings["WepApiURLtoLoad"].ToString();
-        private string _wepApiURLtoDownload = ConfigurationManager.AppSettings["WepApiURLtoDownload"].ToString();
-        private string _wepApiURLExistsFile = ConfigurationManager.AppSettings["WepApiURLExistsFile"].ToString();
-        private string _singalRHost = ConfigurationManager.AppSettings["SignalRHost"].ToString();
-        FileSystemWatcher _watcher;
+        //private string _syncFolder = ConfigurationManager.AppSettings["SyncFolder"].ToString();
+        //private string _webApiURLtoLoad = ConfigurationManager.AppSettings["WepApiURLtoLoad"].ToString();
+        //private string _wepApiURLtoDownload = ConfigurationManager.AppSettings["WepApiURLtoDownload"].ToString();
+        //private string _wepApiURLExistsFile = ConfigurationManager.AppSettings["WepApiURLExistsFile"].ToString();
+        //private string _singalRHost = ConfigurationManager.AppSettings["SignalRHost"].ToString();
+        //FileSystemWatcher _watcher;
 
-        public System.Threading.Thread Thread { get; set; }
-        public IHubProxy Proxy { get; set; }
-        public HubConnection Connection { get; set; }
-        public bool Active { get; set; }
+        //public System.Threading.Thread Thread { get; set; }
+        //public IHubProxy Proxy { get; set; }
+        //public HubConnection Connection { get; set; }
+        //public bool Active { get; set; }
 
         private async void ActionWindowLoaded(object sender, RoutedEventArgs e)
         {
-            Active = true;
-            Thread = new System.Threading.Thread(() =>
-            {
-                Connection = new HubConnection(_singalRHost);
-                Connection.Error += Connection_Error;
-                Connection.StateChanged += Connection_StateChanged;
-                Proxy = Connection.CreateHubProxy("FileSyncHub");
+            //Active = true;
+            //Thread = new System.Threading.Thread(() =>
+            //{
+            //    Connection = new HubConnection(_singalRHost);
+            //    Connection.Error += Connection_Error;
+            //    Connection.StateChanged += Connection_StateChanged;
+            //    Proxy = Connection.CreateHubProxy("FileSyncHub");
 
-                Proxy.On<string, string>("NewFileNotification", (fileName, CRC) => OnNewFileNotified(fileName,CRC));
+            //    Proxy.On<string, string>("NewFileNotification", (fileName, CRC) => OnNewFileNotified(fileName,CRC));
 
-                Connection.Start();
+            //    Connection.Start();
 
-                while (Active)
-                {
-                    System.Threading.Thread.Sleep(10);
-                }
-            })
-            { IsBackground = true };
-            Thread.Start();
+            //    while (Active)
+            //    {
+            //        System.Threading.Thread.Sleep(10);
+            //    }
+            //})
+            //{ IsBackground = true };
+            //Thread.Start();
         }
 
-        private void OnNewFileNotified(string fileName, string CRC)
-        {
-            //Check if I have the File
-            if(!alreadyHaveFile(fileName, CRC))
-                //Download the New File
-                getFile(fileName);
-        }
+        //private void OnNewFileNotified(string fileName, string CRC)
+        //{
+        //    //Check if I have the File
+        //    if(!alreadyHaveFile(fileName, CRC))
+        //        //Download the New File
+        //        getFile(fileName);
+        //}
 
-        private void Connection_StateChanged(StateChange obj)
-        {
-            Dispatcher.Invoke(() => infoLabel.Content = "Status: " + obj.NewState);
-            //MessageBox.Show("State: " + obj.NewState);
-        }
+        //private void Connection_StateChanged(StateChange obj)
+        //{
+        //    Dispatcher.Invoke(() => infoLabel.Content = "Status: " + obj.NewState);
+        //    //MessageBox.Show("State: " + obj.NewState);
+        //}
 
-        private void Connection_Error(Exception obj)
-        {
-            Dispatcher.Invoke(() => infoLabel.Content = "Connection error: " + obj.ToString());
-            //MessageBox.Show("Connection error: " + obj.ToString());
-        }
+        //private void Connection_Error(Exception obj)
+        //{
+        //    Dispatcher.Invoke(() => infoLabel.Content = "Connection error: " + obj.ToString());
+        //    //MessageBox.Show("Connection error: " + obj.ToString());
+        //}
 
         public MainWindow()
         {
-            InitializeComponent();
+            //InitializeComponent();
 
-            _watcher = new FileSystemWatcher();
-            _watcher.Path = _syncFolder;
-            _watcher.NotifyFilter = NotifyFilters.LastWrite;
-            _watcher.Filter = "*.*";
-            _watcher.Changed += new FileSystemEventHandler(OnWatcherChanged);
-            _watcher.Deleted += new FileSystemEventHandler(OnWatcherDeleted);
-            _watcher.EnableRaisingEvents = true;
+            //_watcher = new FileSystemWatcher();
+            //_watcher.Path = _syncFolder;
+            //_watcher.NotifyFilter = NotifyFilters.LastWrite;
+            //_watcher.Filter = "*.*";
+            //_watcher.Changed += new FileSystemEventHandler(OnWatcherChanged);
+            //_watcher.Deleted += new FileSystemEventHandler(OnWatcherDeleted);
+            //_watcher.EnableRaisingEvents = true;
         }
 
-        private void btnSendTest_Click(object sender, RoutedEventArgs e)
-        {
-            var filename = "test.txt";
+        //private void btnSendTest_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var filename = "test.txt";
 
-            sendFile(filename);
-        }
+        //    sendFile(filename);
+        //}
 
-        private bool alreadyHaveFile(string fileName,string CRC)
-        {
-            //Include CRC check ************************************************************************** TO DO
-            return File.Exists(_syncFolder + "\\" + fileName);
-        }
+        //private bool alreadyHaveFile(string fileName,string CRC)
+        //{
+        //    //Include CRC check ************************************************************************** TO DO
+        //    return File.Exists(_syncFolder + "\\" + fileName);
+        //}
 
-        private async void getFile(string fileName)
-        {
-            using (var client = new HttpClient())
-            {
-                var responseStream = await client.GetStreamAsync(_wepApiURLtoDownload +"?fileName="+ fileName);
-                using (var fileWritter = File.Create(_syncFolder+"\\"+fileName))
-                {
-                    //responseStream.Seek(0, SeekOrigin.Begin);
-                    responseStream.CopyTo(fileWritter);
-                }
-            }
-        }
-        private async Task sendFile(string fileName)
-        {
-            byte[] byteFile = tryToReadFile(_syncFolder + "\\" + fileName);
-            HttpContent fileContent = new ByteArrayContent(byteFile);
+        //private async void getFile(string fileName)
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        var responseStream = await client.GetStreamAsync(_wepApiURLtoDownload +"?fileName="+ fileName);
+        //        using (var fileWritter = File.Create(_syncFolder+"\\"+fileName))
+        //        {
+        //            //responseStream.Seek(0, SeekOrigin.Begin);
+        //            responseStream.CopyTo(fileWritter);
+        //        }
+        //    }
+        //}
+        //private async Task sendFile(string fileName)
+        //{
+        //    byte[] byteFile = tryToReadFile(_syncFolder + "\\" + fileName);
+        //    HttpContent fileContent = new ByteArrayContent(byteFile);
             
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var response = await client.PostAsync(_webApiURLtoLoad, new MultipartFormDataContent()
-                {
-                    { new StringContent(fileName),"fileName"},
-                    { fileContent, "file", fileName }
-                });
-            }
-        }
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //        var response = await client.PostAsync(_webApiURLtoLoad, new MultipartFormDataContent()
+        //        {
+        //            { new StringContent(fileName),"fileName"},
+        //            { fileContent, "file", fileName }
+        //        });
+        //    }
+        //}
 
-        private async Task<bool> fileExistsOnServer(string fileName, string CRC)
-        {
-            bool final;
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(_wepApiURLExistsFile + "?fileName=" + fileName);
-                var result = await response.Content.ReadAsStringAsync();
-                final = JsonConvert.DeserializeObject<bool>(result);
-            }
+        //private async Task<bool> fileExistsOnServer(string fileName, string CRC)
+        //{
+        //    bool final;
+        //    using (var client = new HttpClient())
+        //    {
+        //        var response = await client.GetAsync(_wepApiURLExistsFile + "?fileName=" + fileName);
+        //        var result = await response.Content.ReadAsStringAsync();
+        //        final = JsonConvert.DeserializeObject<bool>(result);
+        //    }
 
-            return final;
-        }
+        //    return final;
+        //}
 
-        private byte[] tryToReadFile(string file, int tries = 0)
-        {
-            if (tries == 100)
-                throw new Exception("Can't access file "+file);
+        //private byte[] tryToReadFile(string file, int tries = 0)
+        //{
+        //    if (tries == 100)
+        //        throw new Exception("Can't access file "+file);
 
-            byte[] b;
-            try
-            {
-                b = System.IO.File.ReadAllBytes(file);
-            }
-            catch (IOException)
-            {
-                Thread.Sleep(500);
-                return tryToReadFile(file, ++tries);
-            }
+        //    byte[] b;
+        //    try
+        //    {
+        //        b = System.IO.File.ReadAllBytes(file);
+        //    }
+        //    catch (IOException)
+        //    {
+        //        Thread.Sleep(500);
+        //        return tryToReadFile(file, ++tries);
+        //    }
 
-            return b;
-        }
+        //    return b;
+        //}
 
-        private void OnWatcherChanged(object source, FileSystemEventArgs e)
-        {
-            if (!fileExistsOnServer(e.Name, "").Result)
-                Task.WaitAll(sendFile(e.Name));
-        }
+        //private void OnWatcherChanged(object source, FileSystemEventArgs e)
+        //{
+        //    if (!fileExistsOnServer(e.Name, "").Result)
+        //        Task.WaitAll(sendFile(e.Name));
+        //}
 
-        private void OnWatcherDeleted(object source, FileSystemEventArgs e)
-        {
-            //if (!fileExists(e.Name, "").Result)
-            //    Task.WaitAll(sendFile(e.Name));
-        }
+        //private void OnWatcherDeleted(object source, FileSystemEventArgs e)
+        //{
+        //    //if (!fileExists(e.Name, "").Result)
+        //    //    Task.WaitAll(sendFile(e.Name));
+        //}
 
         private string calculateMD5(string file)
         {
@@ -191,6 +191,11 @@ namespace ClientWatcher
                     return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
                 }
             }
+        }
+
+        private void btnSendTest_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
